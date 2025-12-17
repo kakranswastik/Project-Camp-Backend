0263c96f94be9d7724c726817b23866184dcd6b6
Swastik Kakran <kakranswastik2007@gmail.com>
2025-12-18 01:49:52 +0530

    Send email to user

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,6 +1,42 @@
 import { text } from "express";
 import Mailgen from "mailgen";
-import { act } from "react";
+import nodemailer from "nodemailer"
+
+const sendEmail = async function (options) {
+    const mailGenrator = new Mailgen({
+        theme: "default",
+        product: {
+            name: "Task Manager",
+            link: "https://taskmanagerlink.com"
+        }
+    })
+    const emailTextual = mailGenrator.generatePlaintext(options.mailgenContent)
+    const emailHTML = mailGenrator.generate(options.mailgenContent)
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port: MAILTRAP_SMTP_PORT,
+        auth: {
+            user: MAILTRAP_SMTP_USER,
+            pass: MAILTRAP_SMTP_PASS
+        }
+    })
+
+    const mail = {
+        from: "mail.task@example.com",
+        to: options.email,
+        subject: options.subject,
+        text: emailTextual,
+        html: emailHTML
+    }
+
+    try {
+        await transporter.sendMail(mail)
+    } catch (error) {
+        console.error("Email service failed silently. Make sure that you have provided your MAILTRAP credentials in the .env file")
+        console.error("Error: ", error)
+    }
+} 
 
 const emailVerificationMailgenContent = (username, verificationUrl) => {
   return {
