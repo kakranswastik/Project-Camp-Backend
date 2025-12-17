4cd70d45c62520cfede088073de1530b39bbbd42
Swastik Kakran <kakranswastik2007@gmail.com>
2025-12-18 01:14:47 +0530

    create access, refresh, and temporary token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,81 +1,115 @@
 import mongoose, { schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new schema(
-    {
-        avatar: {
-            type: {
-                url: String,
-                localPath: String,
-            },
-            default: {
-                url: "../../public/images/1148558711187545960.jpg"
-            }
- 
-        },
-
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-            index: true
-        },
-        
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true
-        },
-
-        password: {
-            type: String,
-            required: [true, "Password is required"]
-        },
-
-        isEmailVerified: {
-            type: Boolean,
-            default: false,
-        },
-
-        refreshToken: {
-            type: String
-        },
-
-        forgotPasswordToken: {
-            type: String
-        },
-
-        forgotPasswordExpiry: {
-            type: Date
-        },
-
-        emailVerificationToken: {
-            type: String
-        },
-
-        isEmailVerificationExpiry: {
-            type: Date
-        }
-    }, {
-        timestamps: true
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: "../../public/images/1148558711187545960.jpg",
+      },
     },
+
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    refreshToken: {
+      type: String,
+    },
+
+    forgotPasswordToken: {
+      type: String,
+    },
+
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+
+    emailVerificationToken: {
+      type: String,
+    },
+
+    isEmailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next()
+  if (!this.isModified("password")) return next();
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
-
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password)
-}
+  return await bcrypt.compare(password, this.password);
+};
 
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+  );
+};
 
-export const User = mongoose.model("User", userSchema); 
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
+  );
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+  const haskedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+  const tokenExpiry = Date.now() + 20 * 60 * 1000;
+  return { unHashedToken, haskedToken, tokenExpiry };
+};
+
+export const User = mongoose.model("User", userSchema);
